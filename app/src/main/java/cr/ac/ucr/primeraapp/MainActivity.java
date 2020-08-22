12118f3626015cdc056522a476a596816bca93a6
Makkpas <axisfs12@gmail.com>
2020-08-22 00:08:21 -0600

    Se agregó el FAB para agregar TODOS y un Dialog para ingresar el TODO

--- app/src/main/java/cr/ac/ucr/primeraapp/MainActivity.java
+++ app/src/main/java/cr/ac/ucr/primeraapp/MainActivity.java
@@ -1,17 +1,24 @@
 package cr.ac.ucr.primeraapp;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 import cr.ac.ucr.primeraapp.utils.AppPreferences;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 
     @Override
@@ -50,4 +57,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.fab_add_todo:
+                showAlert();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void showAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        LayoutInflater inflater = getLayoutInflater();
+        final View view = inflater.inflate(R.layout.dialog_add_task, null);
+
+        builder.setView(view)
+                .setPositiveButton(R.string.add, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        TextInputEditText etTaskName = view.findViewById(R.id.et_task_name);
+
+
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null);
+
+        builder.create();
+        builder.show();
+    }
 }
